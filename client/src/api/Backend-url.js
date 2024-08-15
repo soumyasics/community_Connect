a62aec3fd279715004df3387a73f0f8f77a5064f
Anand R P <anandrp.sics@gmail.com>
2024-08-15 15:21:42 +0530

    new build

--- client/src/api/Backend-url.js
+++ client/src/api/Backend-url.js
@@ -1,3 +1,3 @@
-// const BASE_URL = "http://hybrid.srishticampus.in:4023/";
-const BASE_URL = "http://localhost:4023/";
+const BASE_URL = "http://hybrid.srishticampus.in:4023/";
+// const BASE_URL = "http://localhost:4023/";
 export default BASE_URL;
